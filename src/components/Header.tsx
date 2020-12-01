09a8bece9d79a30eec2cde68b3d90102b779c5c3
Danisimogg <angrydaniall123456@gmail.com>
2020-12-01 18:22:02 +0300

    chore:(WIP)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,48 @@
-import { Link } from "gatsby"
-import React from "react"
-
-export interface IHeaderProps
-{
+import { Link } from "gatsby";
+import React from "react";
+import styled from 'styled-components';
+import { FaBars } from 'react-icons/fa';
+export interface IHeaderProps {
     siteTitle: string;
 }
 
-export class Header extends React.Component<IHeaderProps, {}>
-{
-    constructor(props: IHeaderProps)
-    {
-        super(props);
-    }
+const Header = () => {
+    return (
+        <Nav> <NavLink to='/'>Lorem </NavLink>
+            <Bars />
+        </Nav>
 
-    public render(): JSX.Element
-    {
-        return (
-            <header className="site-header">
-                <div className="content">
-                    <div className="logo">
-                        <h1>
-                            <Link to="/">
-                                {this.props.siteTitle}
-                            </Link>
-                        </h1>
-                    </div>
-                    <nav>
-                        <Link to="/">Home</Link>
-                        <Link to="/page-2/">Page 2</Link>
-                    </nav>
-                </div>
-            </header>
-        );
-    }
-}
+    );
+};
+export { Header };
+
+const Nav = styled.nav`
+background:transparent;
+height:80px;
+display:flex;
+justify-content:space-between;
+padding:0.5rem calc((100vw - 1300px)/2);
+
+`;
+
+const NavLink = styled(Link)`
+color:80px;
+display:flex;
+justify-content:space-between;
+padding:0.5rem calc((100vw - 1300px)/2);
+z-index:100;
+position:relative;
+`;
+
+const Bars = styled(FaBars)`
+display:none;
+color:#fff;
+@media screen and (max-width:768px){
+    display:block;
+    position:absolute;
+    top:0;
+    right:0;
+    transform:translate(-100%,75%);
+    font-size:1.8rem;
+    cursor:pointer;
+}`;
