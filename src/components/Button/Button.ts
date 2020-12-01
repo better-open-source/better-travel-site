d0c83d844a459d8b9a4d4d6ec57b6b981a0a9e35
Danyill (Danisimo) <angrydaniall123456@gmail.com>
2020-12-01 20:12:04 +0200

    feat:(added btn component)

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+const Button = styled(Link) < { primary: boolean, big?: boolean, round: boolean, } > `
+background: ${({ primary }) => (primary ? "#F26A2E" : "#077BF1")};
+white-space: nowrap;
+padding: ${({ big }) => (big ? "16px 40px" : "10px 32px")};
+color: #fff;
+font-size: ${({ big }) => (big ? "20px" : "16px")};
+outline: none;
+border: none;
+min-width: 100px;
+cursor: pointer;
+text-decoration: none;
+transition: 0.3s!important;
+border-radius: ${({ round }) => (round ? "50px" : "none")};
+
+ &:hover{
+     background: ${({ primary }) => (primary ? "#077BF1" : "#F26A2E")};
+     transform: translateY(-2px);
+ }
+`;
+
+Button.defaultProps = {
+    big: false,
+};
+export {
+    Button
+};
