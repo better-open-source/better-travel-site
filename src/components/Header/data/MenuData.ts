938675b802b52e3450cd6d438d4a2ef256ed8603
Danyill(Danisimo) <daniall.husacov@gmail.com>
2021-01-24 00:34:14 +0200

    chore:(reactored old components , and extended .gitignore file)

--- src/components/Header/data/MenuData.ts
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-  { title: 'About', link: '/about' },
-  { title: 'Trips', link: '/trips' },
-  { title: 'Bot', link: '/bot' },
-  { title: 'Contact', link: '/contact' },
-];
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import { Header } from './Header';
-import { Layout } from './Layout';
-import { SEO } from './Seo';
-import { Button } from './Button';
-import { Hero } from './Hero';
-import { Dropdown } from './Dropdown';
-import { Email } from './Email';
-
-export { Hero, SEO, Layout, Header, Button, Dropdown, Email };
